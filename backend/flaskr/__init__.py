302e865d7b3eaf337103dbf5db13c36cb3e04ba6
Tsitohaina Rakotondramasy <tsitohaina.toetra.rak@gmail.com>
2022-10-01 23:33:37 -0400

    removed test

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -52,7 +52,7 @@ def create_app(test_config=None):
     @app.route('/categories', methods=["GET"])
     def retrieve_categories():
         categories = Category.query.order_by(Category.id).all()
-        
+        print(categories)
         if len(categories) == 0:
             abort(404)
 
